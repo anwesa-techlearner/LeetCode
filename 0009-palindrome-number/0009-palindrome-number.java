021d0740bb61a71350d9a4928a6f0c4e85ff3a34
Anwesa Ray <156700076+anwesa-techlearner@users.noreply.github.com>
2025-08-25 11:51:37 +0530

    Time: 5 ms (65.73%), Space: 43.9 MB (76.02%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,17 +1,14 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) {
+        if(x<0){
             return false;
         }
-
-        int reverse = 0;
-        int xcopy = x;
-
-        while (x > 0) {
-            reverse = (reverse * 10) + (x % 10);
-            x /= 10;
+        int temp = x;
+        int rev = 0;
+        while(temp!=0){
+            rev = rev*10 + temp%10;
+            temp/=10;
         }
-
-        return reverse == xcopy;        
+        return(rev==x);
     }
 }
